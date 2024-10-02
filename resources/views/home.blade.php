367817222b1aabc2919bce32e8c5cc9eb4775420
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-02 16:57:10 +0200

    [UPD]Add jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
-
 @section('content')
-    <div class="container">
+    <div id="home">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">template</h1>
+                <h1 class="text-center">Pixel GDR World</h1>
+                <p class="text-center">Immergiti nel nostro fantastico mondo in pixel art</p>
+                <div class="text-center">
+                    <a href="#" class="btn-start">Inizia</a>
+                </div>
             </div>
         </div>
     </div>
