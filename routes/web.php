253f23b361b21d80f375ee4ff43f2749452fceaf
SimoneDUrso <simone.durso98@gmail.com>
2024-10-02 18:41:59 +0200

    [UPD] Created HomeController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CharacterController as CharacterController;
-
 use App\Http\Controllers\HomeController as HomeController;
 /*
 |--------------------------------------------------------------------------
