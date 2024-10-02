6d48b24f288e86723b25a1643d70b395d1e9a2be
SimoneDUrso <simone.durso98@gmail.com>
2024-10-02 18:37:08 +0200

    [UPD] Created route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< HEAD
-    <div id="home">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center">Pixel GDR World</h1>
-                <p class="text-center">Immergiti nel nostro fantastico mondo in pixel art</p>
-                <div class="text-center">
-                    <a href="{{ route('homepage') }}" class="btn-start">Inizia</a>
-=======
     <div class="container">
         <div id="home">
             <div class="row">
@@ -16,9 +7,8 @@
                     <h1 class="text-center">Pixel GDR World</h1>
                     <p class="text-center">Immergiti nel nostro fantastico mondo in pixel art</p>
                     <div class="text-center">
-                        <a href="#" class="btn-start">Inizia</a>
+                        <a href="{{ route('homepage') }}" class="btn-start">Inizia</a>
                     </div>
->>>>>>> c49994c296b328417345779d8d93c123934d5b84
                 </div>
             </div>
         </div>
