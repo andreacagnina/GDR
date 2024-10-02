7020ae692000d7ec38f840c242c60fc3cedc17fb
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-02 18:28:49 +0200

    [UPD]Add img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <div id="home">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center">Pixel GDR World</h1>
-                <p class="text-center">Immergiti nel nostro fantastico mondo in pixel art</p>
-                <div class="text-center">
-                    <a href="#" class="btn-start">Inizia</a>
+    <div class="container">
+        <div id="home">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">Pixel GDR World</h1>
+                    <p class="text-center">Immergiti nel nostro fantastico mondo in pixel art</p>
+                    <div class="text-center">
+                        <a href="#" class="btn-start">Inizia</a>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,39 @@
 <header>
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark " id="ftco-navbar">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">GCDR</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-                aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="oi oi-menu"></span>Menu</button>
-            <div class="collapse navbar-collapse d-flex justify-content-end" id="ftco-nav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active"><a href="{{ route('homepage') }}" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="" class="nav-link">About Us</a></li>
-                    <li class="nav-item"><a href="" class="nav-link">Characters</a></li>
-                    <li class="nav-item"><a href="" class="nav-link">Items</a></li>
-                </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark px-5" id="ftco-navbar"
+                    style="height: 60px;">
+                    <a class="navbar-brand d-flex align-items-center" href="index.html" style="height: 60px;">
+                        <h3 class="m-0" style="line-height: 60px;">GCDR</h3>
+                    </a>
+                    <div class="collapse navbar-collapse d-flex justify-content-end align-items-center" id="ftco-nav"
+                        style="height: 60px;">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a href="{{ route('homepage') }}" class="nav-link" style="line-height: 60px;">
+                                    <h4 class="m-0">Home</h4>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link" style="line-height: 60px;">
+                                    <h4 class="m-0">About Us</h4>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link" style="line-height: 60px;">
+                                    <h4 class="m-0">Characters</h4>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link" style="line-height: 60px;">
+                                    <h4 class="m-0">Items</h4>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
-    </nav>
+    </div>
 </header>
