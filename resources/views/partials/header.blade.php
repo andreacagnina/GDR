ebb7f32108ac1ec6ef1a4d8c7f974b2c3be01f6f
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-03 10:48:22 +0200

    [UPD] Added class active on currentRouteName()

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,14 +4,14 @@
             <div class="col-12">
                 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark px-5" id="ftco-navbar"
                     style="height: 60px;">
-                    <a class="navbar-brand d-flex align-items-center" href="index.html" style="height: 60px;">
+                    <a class="navbar-brand d-flex align-items-center" href="{{ route('homepage') }}" style="height: 60px;">
                         <h3 style="line-height: 60px;">GCDR</h3>
                     </a>
                     <div class="collapse navbar-collapse d-flex justify-content-end align-items-center" id="ftco-nav"
                         style="height: 60px;">
                         <ul class="navbar-nav">
                             <li class="nav-item active">
-                                <a href="{{ route('homepage') }}" class="nav-link" style="line-height: 60px;">
+                                <a href="{{ route('homepage') }}" class="{{ Route::currentRouteName() === 'homepage' ? 'active' : ''}} nav-link" style="line-height: 60px;">
                                     <h4>Home</h4>
                                 </a>
                             </li>
@@ -21,7 +21,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="{{ route('characters.index') }}" class="nav-link" style="line-height: 60px;">
+                                <a href="{{ route('characters.index') }}" class="{{ Route::currentRouteName() === 'characters.index' ? 'active' : ''}} nav-link" style="line-height: 60px;">
                                     <h4>Characters</h4>
                                 </a>
                             </li>
