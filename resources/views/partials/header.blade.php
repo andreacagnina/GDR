2bc803bce0480e2db7415f125302939b84016edf
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-10-03 11:07:22 +0200

    [UPD] Added some style to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,33 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark px-5" id="ftco-navbar"
-                    style="height: 60px;">
-                    <a class="navbar-brand d-flex align-items-center" href="{{ route('homepage') }}" style="height: 60px;">
-                        <h3 style="line-height: 60px;">GCDR</h3>
-                    </a>
-                    <div class="collapse navbar-collapse d-flex justify-content-end align-items-center" id="ftco-nav"
-                        style="height: 60px;">
+                <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark px-5" id="ftco-navbar">
+                    <a class="navbar-brand d-flex align-items-center" href="{{ route('homepage') }}"><h3>GCDR</h3></a>
+                    <div class="collapse navbar-collapse d-flex justify-content-end align-items-center" id="ftco-nav">
                         <ul class="navbar-nav">
                             <li class="nav-item active">
-                                <a href="{{ route('homepage') }}" class="{{ Route::currentRouteName() === 'homepage' ? 'active' : ''}} nav-link" style="line-height: 60px;">
-                                    <h4>Home</h4>
-                                </a>
+                                <a href="{{ route('homepage') }}" class="{{ Route::currentRouteName() === 'homepage' ? 'active' : ''}} nav-link"><h4>Home</h4></a>
                             </li>
                             <li class="nav-item">
-                                <a href="" class="nav-link" style="line-height: 60px;">
-                                    <h4>AboutUs</h4>
-                                </a>
+                                <a href="{{ route('characters.index') }}" class="{{ Route::currentRouteName() === 'characters.index' ? 'active' : ''}} nav-link"><h4>Characters</h4></a>
                             </li>
                             <li class="nav-item">
-                                <a href="{{ route('characters.index') }}" class="{{ Route::currentRouteName() === 'characters.index' ? 'active' : ''}} nav-link" style="line-height: 60px;">
-                                    <h4>Characters</h4>
-                                </a>
+                                <a href="" class="nav-link"><h4>Items</h4></a>
                             </li>
                             <li class="nav-item">
-                                <a href="" class="nav-link" style="line-height: 60px;">
-                                    <h4>Items</h4>
-                                </a>
+                                <a href="" class="nav-link"><h4>AboutUs</h4></a>
                             </li>
                         </ul>
                     </div>
