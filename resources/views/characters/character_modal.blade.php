4008549db0036bac8771a8ec1e2919570b0e200d
andreacagnina <amirkal@libero.it>
2024-10-06 13:41:39 +0200

    [feat] delete

--- resources/views/characters/character_modal.blade.php
+++ resources/views/characters/character_modal.blade.php
@@ -1,17 +1,17 @@
 <div class="modal fade" id="deleteCharacterModal" tabindex="-1" aria-labelledby="deleteCharacterModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
+    <div class="modal-dialog ">
+        <div class="modal-content bg-dark">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="deleteCharacterModalLabel">Eliminazione del progetto corrente</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h1 class="modal-title fs-5 text-danger" id="deleteCharacterModalLabel">Eliminazione del personaggio
+                    selezionato</h1>
             </div>
-            <div class="modal-body">
-                Sei sicuro di voler cancellare questo progetto?
+            <div class="modal-body text-white" id="modal_text">
+                Sei sicuro di voler cancellare questo personaggio?
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                <button type="button" class="btn btn-primary confirm-delete">Elimina</button>
+                <button type="button" class="btn btn-danger confirm-delete">Elimina</button>
             </div>
         </div>
     </div>
