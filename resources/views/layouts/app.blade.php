b943d6e4d8fd38252582c4d0dbaf5a536cb557ac
SimoneDUrso <simone.durso98@gmail.com>
2024-10-06 14:29:58 +0200

    [UPD] Modify link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="path/to/your/javascript/file.js"></script>
+    {{-- <script src="path/to/your/javascript/file.js"></script> --}}
 </body>
 
 </html>
