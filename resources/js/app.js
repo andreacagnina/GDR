3fe3560a94153bb48120cb17a481c1a53bdddfbd
andreacagnina <amirkal@libero.it>
2024-10-11 17:07:16 +0200

    [feat] remove timoute

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,14 +29,6 @@ buttons.forEach((button) => {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
 
-    var successAlert = document.getElementById('success-alert');
 
-    if (successAlert) {
-        setTimeout(function () {
-            successAlert.style.display = 'none';;
-        }, 4000);
-    }
 
-});
